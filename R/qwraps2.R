f74a9a4ad50e64dcc3e7c99e6dca72b4ff1625ce
Peter DeWitt (inspiron) <dewittpe@gmail.com>
2015-04-01 00:06:11 -0600

    added package documentation file

---
+++ R/qwraps2.R
@@ -0,0 +1,19 @@
+#' A collection of wrapper functions for aiding the authoring of reproducible
+#' reports.
+#'
+#' \pkg{qwraps2} is a collection of functions I have found very useful when
+#' working on a varied collection of different analysis reports.  These
+#' functions are commonly used wrappers which I have been reusing and modifying
+#' over several years.
+#'
+#' Several wrappers for \pkg{ggplot2} style graphics, such as ROC, AUC, and KM
+#' plots are provided.  Named as \code{\link{qroc}}, \code{\link{qauc}}, and
+#' \code{\link{qkmplot}} to pay homage to \code{qplot} form \pkg{ggplot2} and
+#' the standard names for such plots.
+#'
+#' Other functions are used to quickly generate meaningful character strings for
+#' outputting results in .Rnw, .Rmd, or other similar functions.
+#'
+#' @docType package
+#' @name qwraps2
+NULL
