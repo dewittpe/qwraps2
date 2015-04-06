93f2162fa88200f80296ace51ee4064d34daf27b
Peter DeWitt (inspiron) <dewittpe@gmail.com>
2015-04-05 23:18:00 -0600

    Initial work for issue #5

---
+++ R/n_perc.R
@@ -0,0 +1,52 @@
+#' @title Count and Percentage
+#'
+#' @description A function for calculating and formatting counts and
+#' percentages.
+#'
+#' @details
+#' Default behavior will return the count of successes and the percentage as "N
+#' (pp%).  If there are missing values in the input vector the omission of such
+#' can be controlled by setting \code{na.rm = TRUE}.  In this case, the number
+#' of non-missing values will be reported by default.  Omission of the
+#' non-missing values can be controlled by setting \code{show_denom = "never"}.
+#' 
+#'
+#' @param x a 0:1 or boolean vector
+#' @param digits digits to the right of the decimal point to return in the
+#' percentage estimate.
+#' @param na_rm if true, omit NA values
+#' @param show_denom defaults to "ifNA".  Other options are "always" or "never".
+#' @param markup latex or markdown
+#'
+#' @return a character vector of the formatted values
+#'
+#' @examples
+#' 
+#' n_perc(c(0, 1,1, 1, 0, 0), show_denom = "always")
+#' n_perc(c(0, 1,1, 1, 0, 0, NA), na_rm = T)
+#' 
+#' n_perc(mtcars$cyl == 6)
+#'
+#' @export   
+n_perc <- function(x, 
+                   digits = getOption("qwraps2_frmt_digits", 2), 
+                   na_rm = FALSE, 
+                   show_denom = "ifNA", 
+                   markup = getOption("qwraps2_markup", "latex")) { 
+  d <- sum(!is.na(x))
+  n <- sum(x, na.rm = na_rm)
+  p <- frmt(100 * n/d, digits)
+
+  if (show_denom =="always" | any(is.na(x))) { 
+    rtn <- paste0(as.integer(n), "/", as.integer(d), " (", p, "%)")
+  } else { 
+    rtn <- paste0(as.integer(n), " (", p, "%)")
+  }
+
+  if (markup == "latex") { 
+    rtn <- gsub("%", "\\\\%", rtn)
+  } 
+
+  return(rtn)
+}
+
