eaca5f0edbda858a6017d1a741713e0a20182d6f
Peter DeWitt (inspiron) <dewittpe@gmail.com>
2015-04-05 22:06:00 -0600

    Formatting of p-values is set up.  See issue #1.

--- R/frmtp.R
+++ R/frmtp.R
@@ -8,15 +8,16 @@
 #' requirements.  As I work on papers aimed at different journals, the
 #' formatting functions will be extended to match.
 #'
-#' Default settings are controlled through \code{options()}.
+#' Default settings are controlled through the function arguments but should be
+#' set via \code{options()}.
 #' 
 #' Default settings report the P-value exactly if P >
 #' \code{getOptions("qwraps2_frmtp_digits, 2)} and reports ''P <
 #' \code{10^-(getOptions("qwraps2_frmtp_digits, 2))} otherwise.  By the leading
 #' zero is controlled via \code{getOptions("qwraps2_frmpt_leading0", TRUE)} and
-#' a captial or lower case P is controled by
+#' a upper or lower case P is controlled by
 #' \code{getOptions("qwraps2_frmpt_case", "upper")}.
-#' These options are ignored if \code{style != "Default"}.
+#' These options are ignored if \code{style != "default"}.
 #'
 #' Journals with predefined P-value formatting
 #' 
@@ -33,24 +34,78 @@
 #' \code{\link{frmt}}
 #'
 #' @param x a vector of P-values to format
-frmtp <- function(x) {  
-  # sytle <- getOption("qwraps2_journal", "Default")
-  # markup <-  getOption("qwraps2_markup", "latex")
-  f <- match.fun(paste0("frmtp_"), sytle)
-  f(x) 
-}
-
-frmtp_defualt <- function(x) { 
-  p_cutoff <- 10^-getOptions("qwraps2_frmtp_digits", 2)
-  
-  if (x < p_cutoff) { 
-    p_val <- "< .01"
-  } else {
-    p_val <- paste0("= ", formatC(x, digits = getOptions("qwraps2_frmtp_digits", 2), format = "f")) 
+#' @param style a character string indicating a specific journal requirements
+#' for p-value formatting.  
+#' @param markup a character string indicating if the output should be latex or
+#' markup.
+#' @param case a character string indicating if the output should be upper case
+#' or lower case.
+#' @param leading0 boolean, whether or not the p-value should be reported as
+#' 0.0123 (TRUE, default), or .0123 (FALSE).
+#' @param ... not currently implemented.
+#'
+#' @return a character vector of the formatted p-values
+#'
+#' @examples
+#' 
+#' ps <- c(0.2, 0.001, 0.00092, 0.047, 0.034781, 0.0000872, 0.787, 0.05, 0.043)
+#' # LaTeX is the default markup language
+#' cbind("raw"      = ps, 
+#'       "default"  = frmtp(ps), 
+#'       "3lower"   = frmtp(ps, digits = 3, case = "lower"),
+#'       "PediDent" = frmtp(ps, style = "pediatric_dentistry"))
+#'
+#' # Using markdown
+#' cbind("raw"      = ps, 
+#'       "default"  = frmtp(ps, markup = "markdown"), 
+#'       "3lower"   = frmtp(ps, digits = 3, case = "lower", markup = "markdown"),
+#'       "PediDent" = frmtp(ps, style = "pediatric_dentistry", markup = "markdown"))
+#'
+#' @export   
+frmtp <- function(x,
+                  style    = getOption("qwraps2_journal", "default"), 
+                  digits   = getOption("qwraps2_frmtp_digits", 2), 
+                  markup   = getOption("qwraps2_markup", "latex"),
+                  case     = getOption("qwraps2_frmtp_case", "upper"), 
+                  leading0 = getOption("qwraps2_frmpt_leading0", TRUE), 
+                  ...) {  
+  if (style == "default") {
+    rtn <- frmtp_default(x, digits, case, leading0)
+  } else { 
+    f <- match.fun(paste0("frmtp_", style))
+    rtn <- f(x, ...) 
   }
-  p_val
+
+  if (markup == "latex") { 
+    rtn <- paste0("$", rtn, "$")
+  } else if (markup == "markdown") {
+    rtn <- gsub("(P|p)", "\\*\\1\\*", rtn)
+  }
+  return(rtn)
 }
 
+frmtp_default <- function(x, digits, case, leading0) { 
+  p_cutoff <- 10^-digits
+  
+  sapply(x, 
+         function(xx) { 
+           if (xx < p_cutoff) { 
+             p_val <- paste("P <", formatC(p_cutoff, format = "g"))
+           } else {
+             p_val <- paste0("P = ", formatC(xx, digits = digits, format = "f")) 
+           }
+
+           if (case == "lower") { 
+             p_val <- tolower(p_val)
+           }
+
+           if (!leading0) { 
+             p_val <- gsub("0\\.", "\\.", p_val)
+           }
+
+           p_val
+         })
+}
 
 frmtp_pediatric_dentistry <- function(p) { 
   sapply(p, function(x) {
@@ -58,21 +113,13 @@ frmtp_pediatric_dentistry <- function(p) {
          if (x < 0.001) { 
            p_val <- "< .001"
          } else if (x < 0.01) { 
-           p_val <- paste0("= ", formatC(x, digits = 3, format = "f")) %>% 
-                    gsub("0\\.", "\\.", .)
+           p_val <- paste0("= ", formatC(x, digits = 3, format = "f"))
          } else if (x < 0.05 & round(x, 2) == 0.05) { 
-           p_val <- paste0("= ", formatC(x, digits = 3, format = "f")) %>% 
-                    gsub("0\\.", "\\.", .)
+           p_val <- paste0("= ", formatC(x, digits = 3, format = "f"))
          } else { 
-           p_val <- paste0("= ", formatC(x, digits = 2, format = "f")) %>% 
-                    gsub("0\\.", "\\.", .)
-         }
-         paste0("$P ", p_val, "$")
+           p_val <- paste0("= ", formatC(x, digits = 2, format = "f"))
+                             }
+         paste0("P ", gsub("0\\.", "\\.", p_val))
       })
 }
-
-# Test for frmtp
-# ps <- c(0.1, 0.2, 0.001, 0.00092, 0.047, 0.034781, 0.000000872, 0.787887, 0.05,
-#         0.043)
-# cbind(ps, frmtp(ps))
 
