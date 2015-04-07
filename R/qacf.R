e990be1614faf4fe9994f5f13a8a8f45d4498e0d
Peter DeWitt (UCD) <peter.dewitt@ucdenver.edu>
2015-04-07 14:31:53 -0600

    Place holder files for qacf and qkmplot added.

---
+++ R/qacf.R
@@ -0,0 +1,18 @@
+#' @title Autocorrelation plot
+#'
+#' @description TO BE CONSTRUCTED
+#'
+#' @details
+#' TO DO
+#'
+#' @param x object
+#' @param ... other arguments pased to stats::acf
+#'
+#' @return a ggplot.  
+#' 
+#' @export   
+#' @rdname qacf
+qacf <- function(x, ...) { 
+  stop("Function Not Yet Defined")
+  
+} 
---
+++ R/qkmplot.R
@@ -0,0 +1,18 @@
+#' @title Kaplan-Meier Plot
+#'
+#' @description TO BE CONSTRUCTED
+#'
+#' @details
+#' TO DO
+#'
+#' @param x object
+#' @param ... other arguments pased to survival::plot.survfit
+#'
+#' @return a ggplot.  
+#' 
+#' @export   
+#' @rdname qkmplot
+qkmplot <- function(x, ...) { 
+  stop("Function Not Yet Defined")
+  
+} 
