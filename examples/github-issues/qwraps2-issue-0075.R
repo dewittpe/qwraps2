4feae54c54f853a5bfd2c2ce2ac7faceb0726287
Peter DeWitt <peter.dewitt@ucdenver.edu>
2019-04-28 14:58:38 -0600

    update #75 example

--- examples/github-issues/qwraps2-issue-0075.R
+++ examples/github-issues/qwraps2-issue-0075.R
@@ -2,46 +2,46 @@ library(reprex)
 
 reprex({
 
-  # namespaces and options:
-  library(qwraps2)
-  library(magrittr)
-  library(dplyr)
-  options(qwraps2_markup = "markdown")
+library(magrittr)
+library(dplyr)  
+library(qwraps2)
+options(qwraps2_markup="markdown")
 
-  # build an example data set:
-  set.seed(42)
+## Creating dummy data
+group_1    <- rep(c("a", "b"), 5)
+group_2    <- rep(1:2, each=5)
+response_1 <- c(1, 1, 0, 0, 0, 0, 0, 0, 1, 0)
+response_2 <- c(0, 1, 1, 1, 1, 0, 0, 0, 1, 0)
+response_3 <- c(0, 1, 0, 1, 1, 1, 1, 1, 1, 0)
+response_4 <- c(0, 0, 0, 0, 0, 0, 0, 0, 1, 0)
 
-  # I AM GUESING AT THIS FORM IF IT IS NOT CORRECT THEN ALL THAT FOLLOWS WILL BE
-  # WRONG TOO.
-  dat <- tibble(Type1 = factor(sample(LETTERS[1:2], 100, replace = TRUE), levels = LETTERS[1:2]),
-                Type2 = factor(sample(LETTERS[3:4], 100, replace = TRUE), levels = LETTERS[3:5]),
-                Var1  = sample(c(0L, 1L, NA_integer_), 100, replace = TRUE),
-                Var2  = sample(c(0L, 1L, NA_integer_), 100, replace = TRUE),
-                Var3  = sample(c(0L, 1L, NA_integer_), 100, replace = TRUE))
+data <- data.frame(group_1, group_2, response_1, response_2, response_3, response_4)
 
-  print(dat, n = 3)
-  summary(dat)
-
-  # gather the data such that the Var1, Var2, and Var3 columns become a
-  # key/value pairing and filter to only the rows with a value of 1.  That
-  # should match what you suggested for the summary above.
-  dat2 <-
-    dat %>%
-    tidyr::gather(., key = "key", value = "value", Var1:Var3) %>%
-    dplyr::filter(value == 1)
-
-  dat2
-
-  # The qsummary will quickly build the list-of-lists needed
-  qsummary(dat2)$key
-
-  # build the tables and bind together
-  by_type1 <- summary_table(x = group_by(dat2, Type1), summaries = qsummary(dat2)['key'])
-  by_type2 <- summary_table(x = group_by(dat2, Type2), summaries = qsummary(dat2)['key'])
-  whole    <- cbind(by_type1, by_type2)
-
-  # print the result
-  print(whole, rtitle = "summary", booktabs = TRUE)
-},
-outfile = "issue-0075"
+our_summary1 <- list(
+  "Responses 1:2" = list(
+                         "Response 1 and not Response 2"  = ~ qwraps2::n_perc0(response_1 == 1 & response_2 == 0, na_rm = TRUE), 
+                         "Response 2 and not Response 1"  = ~ qwraps2::n_perc0(response_1 == 0 & response_2 == 1, na_rm = TRUE),
+                         "!Response 1 and  Response 2"    = ~ qwraps2::n_perc0(response_1 == 1 & response_2 == 1, na_rm = TRUE),
+                         "Neither Response 1 nor Response 2"    = ~ qwraps2::n_perc0(response_1 == 0 & response_2 == 0, na_rm = TRUE)
+                         ),
+  "Responses 3:4" = list("Response 3"=~ qwraps2::n_perc0(response_3 == 1,na_rm = TRUE),
+                         "Response 4"=~ qwraps2::n_perc0(response_4 == 1,na_rm = TRUE))
 )
+
+all <- summary_table(dplyr::group_by(data, group_1), our_summary1)
+sev <- summary_table(dplyr::group_by(data, group_2), our_summary1)
+whole <- cbind(sev, all)
+print(whole, rtitle="summary", booktabs = TRUE)
+
+})
+
+#> 
+#> 
+#> |summary                 |group_2: 1 (N = 5) |group_2: 2 (N = 5) |group_1: a (N = 5) |group_1: b (N = 5) |
+#> |:-----------------------|:------------------|:------------------|:------------------|:------------------|
+#> |**Responses 1:2**       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |
+#> |&nbsp;&nbsp; Response 1 |2 (40)             |1 (20)             |2 (40)             |1 (20)             |
+#> |&nbsp;&nbsp; Response 2 |4 (80)             |1 (20)             |3 (60)             |2 (40)             |
+#> |**Responses 3:4**       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |&nbsp;&nbsp;       |
+#> |&nbsp;&nbsp; Response 3 |3 (60)             |4 (80)             |3 (60)             |4 (80)             |
+#> |&nbsp;&nbsp; Response 4 |0 (0)              |1 (20)             |1 (20)             |0 (0)              |
