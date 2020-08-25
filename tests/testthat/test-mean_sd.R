0e99a7c4d2f0d16e0b5691a066d315cf7ef6ec57
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-25 09:25:27 -0600

    add test file for gmean_sd

--- tests/testthat/test-mean_sd.R
+++ tests/testthat/test-mean_sd.R
@@ -1,4 +1,9 @@
-test_that("basic formatting", 
+test_that("basic formatting",
           {
             expect_equal(mean_sd(mtcars$mpg), "20.09 $\\pm$ 6.03")
+          })
+
+test_that("warning given if show_n is not as expected",
+          {
+            expect_warning(mean_sd(mtcars$mpg, show_n = TRUE))
           })
