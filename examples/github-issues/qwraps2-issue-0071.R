6e1a90481c26c3b1fd5d2ec82642c8a1e0b8446b
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-24 22:56:34 -0600

    updated example for #71

--- examples/github-issues/qwraps2-issue-0071.R
+++ examples/github-issues/qwraps2-issue-0071.R
@@ -1,43 +1,46 @@
+library(reprex)
+
+reprex({
 #'
-#' A work around, at this time, would be use `interaction` within the `group_by`
-#' call.  The `summary_table` table code assumes that there is only one variable
-#' being used for the grouping.  Here is an example:
-#+ message = FALSE
-library(dplyr)
+#' Version 0.5.0 of qwraps2, which will be published soon, refactored how
+#' `summary_table` was implmented.  The changes allow for grouping by multiple
+#' variables and does not require use of dplyr.
+#'
+#' __NOTE__ use of the data pronoun `.data` in the summary is no longer needed
+#' or recommended.
 library(qwraps2)
 options(qwraps2_markup = "markdown")
+packageVersion("qwraps2")
 
 our_summary <-
   list(
-       "mpg" = list( "min" = ~ min(.data$mpg),
-                    "mean (95% CI)" = ~ frmtci(mean_ci(.data$mpg))),
-       "hp" = list("min" = ~ min(.data$hp),
-                   "max" = ~ max(.data$hp),
-                   "mean (95% CI)" = ~ frmtci(mean_ci(.data$hp)))
+       "mpg" = list( "min" = ~ min(mpg),
+                    "mean (95% CI)" = ~ frmtci(mean_ci(mpg))),
+       "hp" = list("min" = ~ min(hp),
+                   "max" = ~ max(hp),
+                   "mean (95% CI)" = ~ frmtci(mean_ci(hp)))
   )
 
-
-by_color <- summary_table(dplyr::group_by(mtcars, cyl), summaries = our_summary)
-by_cut   <- summary_table(dplyr::group_by(mtcars, am), summaries = our_summary)
+#' Using the refactored summary_table
+by_cyl    <- summary_table(mtcars, our_summary, by = "cyl")
+by_am     <- summary_table(mtcars, our_summary, by = "am")
+by_cyl_am <- summary_table(mtcars, our_summary, by = c("cyl", "am"))
 
 #+ results = "asis"
-summary_table(dplyr::group_by(mtcars, interaction(cyl, am)), summaries = our_summary)
-
+by_cyl
+by_am
+by_cyl_am
 
 #'
-#' To help with the default output it might be helpful to define a variable in
-#' the `data.frame` for the grouping.  This is not dissimilar to how `group_by`
-#' in dplyr v0.8.0 works.
-
-mtcars$Engine <-
-  case_when(
-            mtcars$cyl == 4 & mtcars$am == 0 ~ "Four Cylinders, Manual",
-            mtcars$cyl == 6 & mtcars$am == 0 ~ "Six Cylinders, Manual",
-            mtcars$cyl == 8 & mtcars$am == 0 ~ "Eight Cylinders, Manual",
-            mtcars$cyl == 4 & mtcars$am == 1 ~ "Four Cylinders, Automatic",
-            mtcars$cyl == 6 & mtcars$am == 1 ~ "Six Cylinders, Automatic",
-            mtcars$cyl == 8 & mtcars$am == 1 ~ "Eight Cylinders, Automatic")
+#' Producing the same tables using dplyr::group_by
+grpby_cyl    <- summary_table(dplyr::group_by(mtcars, cyl),     our_summary)
+grpby_am     <- summary_table(dplyr::group_by(mtcars, am),      our_summary)
+grpby_cyl_am <- summary_table(dplyr::group_by(mtcars, cyl, am), our_summary)
 
 #+ results = "asis"
-summary_table(group_by(mtcars, Engine), our_summary)
-
+grpby_cyl
+grpby_am
+grpby_cyl_am
+},
+venue = "gh"
+)
