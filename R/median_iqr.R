be029599ffb54e39519bb254cbc4a0cb6a2782fd
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-25 09:11:16 -0600

    extend examples and test for median_iqr

--- R/median_iqr.R
+++ R/median_iqr.R
@@ -1,6 +1,6 @@
 #' @title Median and Inner Quartile Range
 #'
-#' @description A function for calculating and formatting the median and 
+#' @description A function for calculating and formatting the median and
 #' inner quartile range of a data vector.
 #' #' @details
 #' Given a numeric vector, \code{median_iqr} will return a character string with
@@ -22,34 +22,33 @@
 #' quantile(x, probs = c(1, 3)/4)
 #' median_iqr(x)
 #' median_iqr(x, show_n = "always")
-#' 
+#'
 #' x[187] <- NA
 #' # median_iqr(x) ## Will error
 #' median_iqr(x, na_rm = TRUE)
 #'
-#'
-#' @export   
-median_iqr <- function(x, 
-                       digits = getOption("qwraps2_frmt_digits", 2), 
-                       na_rm = FALSE, 
-                       show_n = "ifNA", 
-                       markup = getOption("qwraps2_markup", "latex")) { 
+#' @export
+median_iqr <- function(x,
+                       digits = getOption("qwraps2_frmt_digits", 2),
+                       na_rm = FALSE,
+                       show_n = "ifNA",
+                       markup = getOption("qwraps2_markup", "latex")) {
   n <- sum(!is.na(x))
   m <- stats::median(x, na.rm = na_rm)
   qs <- stats::quantile(x, probs = c(1, 3) / 4, na.rm = na_rm)
 
   rtn <- paste0(qwraps2::frmt(m, digits), " (",
-                qwraps2::frmt(qs[1L], digits), ", ", 
+                qwraps2::frmt(qs[1L], digits), ", ",
                 qwraps2::frmt(qs[2L], digits), ")")
 
-  if (all(!(show_n %in% c("ifNA", "always", "never")))) { 
+  if (all(!(show_n %in% c("ifNA", "always", "never")))) {
     warning("'show_n' should be in c('ifNA', 'always', 'never').  Setting to 'ifNA'.")
     show_n <- "ifNA"
   }
 
-  if (show_n == "always" | (show_n == "ifNA" & any(is.na(x)))) { 
+  if (show_n == "always" | (show_n == "ifNA" & any(is.na(x)))) {
     rtn <- paste0(qwraps2::frmt(as.integer(n), digits), "; ", rtn)
-  } 
+  }
 
   return(rtn)
 }
--- tests/testthat/test-median_iqr.R
+++ tests/testthat/test-median_iqr.R
@@ -1,4 +1,15 @@
-test_that("basic formatting", 
+test_that("basic formatting",
           {
             expect_equal(median_iqr(mtcars$mpg), "19.20 (15.43, 22.80)")
+          })
+
+test_that("warning given if show_n is not as expected",
+          {
+            expect_warning(median_iqr(mtcars$mpg, show_n = TRUE))
+          })
+
+test_that("missing value will result in an error",
+          {
+            x <- c(1, NA, 2:10)
+            expect_error(median_iqr(x))
           })
