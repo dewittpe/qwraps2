76d2a4e1d2c14658c837ff129e3c4bc16e73642e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-26 13:21:32 -0600

    extend examples for geomtric mean, var, sd

--- R/geometric_mean_sd.R
+++ R/geometric_mean_sd.R
@@ -7,6 +7,36 @@
 #'   stripped before the computation proceeds.
 #' @return a numeric value
 #'
+#' @seealso \code{\link{gmean_sd}} for easy formatting of the geometric mean and
+#' standard deviation.  \code{vignette("summary-statistics", package =
+#' "qwraps2")}.
+#'
+#' @examples
+#'
+#' set.seed(42)
+#' x <- runif(14, min = 4, max = 70)
+#'
+#' # geometric mean - four equivalent ways to get the same result
+#' prod(x) ^ (1 / length(x))
+#' exp(mean(log(x)))
+#' 1.2 ^ mean(log(x, base = 1.2))
+#' gmean(x)
+#'
+#' # geometric variance
+#' gvar(x)
+#'
+#' # geometric sd
+#' exp(sd(log(x)))                                     ## This is wrong (incorrect sample size)
+#' exp(sqrt((length(x) - 1) / length(x)) * sd(log(x))) ## Correct calculation
+#' gsd(x)
+#'
+#' # Missing data will result in and NA being returned
+#' x[c(2, 4, 7)] <- NA
+#' gmean(x)
+#' gmean(x, na_rm = TRUE)
+#' gvar(x, na_rm = TRUE)
+#' gsd(x, na_rm = TRUE)
+#'
 #' @name geometric_mean_var_sd
 NULL
 
