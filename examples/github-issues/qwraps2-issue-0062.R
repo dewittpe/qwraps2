69180931d3d4b9adfec58d7273ff11a169907f2e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-26 17:29:01 -0600

    update example code for #62 to work in the reprex workflow

--- examples/github-issues/qwraps2-issue-0062.R
+++ examples/github-issues/qwraps2-issue-0062.R
@@ -1,4 +1,3 @@
-```r
 library(qwraps2)
 options(qwraps2_markup = "markdown")
 
@@ -11,28 +10,8 @@ summ_stats <-
                                     "max"       = "~ qwraps2::frmt(max(%s))",
                                     "median (IQR)" = "~ qwraps2::median_iqr(%s)"))
 names(summ_stats)
-# [1] "mpg" "wt" 
 
 names(summ_stats)[2] <- "Weight"
 names(summ_stats)
-# [1] "mpg"    "Weight"
 
 summary_table(my_data, summ_stats)
-```
-
-
-|                          |my_data (N = 32)     |
-|:-------------------------|:--------------------|
-|**mpg**                   |&nbsp;&nbsp;         |
-|&nbsp;&nbsp; mean (sd)    |20.09 &plusmn; 6.03  |
-|&nbsp;&nbsp; min          |10.40                |
-|&nbsp;&nbsp; max          |33.90                |
-|&nbsp;&nbsp; median (IQR) |19.20 (15.43, 22.80) |
-|**Weight**                |&nbsp;&nbsp;         |
-|&nbsp;&nbsp; mean (sd)    |3.22 &plusmn; 0.98   |
-|&nbsp;&nbsp; min          |1.51                 |
-|&nbsp;&nbsp; max          |5.42                 |
-|&nbsp;&nbsp; median (IQR) |3.33 (2.58, 3.61)    |
-
-
-
