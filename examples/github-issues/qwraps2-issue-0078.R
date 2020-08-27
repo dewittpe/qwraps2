0169fbba8dabdea7841e7a6874e6acacd7988a38
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-08-27 09:41:12 -0600

    update example for #78

--- examples/github-issues/qwraps2-issue-0078.R
+++ examples/github-issues/qwraps2-issue-0078.R
@@ -1,32 +1,23 @@
-library(reprex)
+library(qwraps2)
+library(dplyr)
+options(qwraps2_markup = "markdown")
 
-reprex(
+my_mtcars <- dplyr::select(mtcars, mpg, cyl, hp)
+stats_summary <- list("MPG" =
+                      list("min" = ~ qwraps2::frmt(min(.data[["mpg"]])),
+                           "max" = ~ qwraps2::frmt(max(.data[["mpg"]]))))
 
-{
-  library(qwraps2)
-  library(dplyr)
-  options(qwraps2_markup = "markdown")
+st <- summary_table(dplyr::group_by(my_mtcars, cyl), stats_summary)
 
-  my_mtcars <- dplyr::select(mtcars, mpg, cyl, hp)
-  stats_summary <- list("MPG" =
-                        list("min" = ~ qwraps2::frmt(min(.data[["mpg"]])),
-                             "max" = ~ qwraps2::frmt(max(.data[["mpg"]]))))
+#'
+#' The default table is
+#'
+st
 
-  st <- summary_table(dplyr::group_by(my_mtcars, cyl), stats_summary)
+#'
+#' First question, is it possible to rename the columns, yes.  One way is to
+#' define the column names via the `cname` argument of the print method for
+#' the summary table.
+#'
+print(st, align = "r")
 
-  #'
-  #' The default table is
-  #'
-  st
-
-  #'
-  #' First question, is it possible to rename the columns, yes.  One way is to
-  #' define the column names via the `cname` argument of the print method for
-  #' the summary table.
-  #'
-  print(st, align = "r")
-
-}
-
-,
-venue = "gh")
