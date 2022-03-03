7a0f936c7a58739f8fbe78b0eb4fa4b4518e9826
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-03-02 17:30:56 -0700

    extend testing of logit and invlogit

--- tests/testthat/test-logit.R
+++ tests/testthat/test-logit.R
@@ -1,4 +1,4 @@
-test_that("logit", 
+test_that("logit",
           {
             set.seed(42)
             p <- runif(100)
@@ -6,9 +6,21 @@ test_that("logit",
             expect_equivalent(logit(p), log(p/(1-p)))
           })
 
-test_that("invlogit", 
+test_that("invlogit",
           {
             set.seed(42)
             p <- runif(100)
             expect_equivalent(invlogit(log(p/(1-p))), p)
           })
+
+test_that("logit and invlogit are equivalent to plogis",
+          {
+            x <- seq(-3, 3, length = 1001)
+            expect_equivalent(invlogit(x), plogis(x))
+
+            x <- seq(sqrt(.Machine$double.eps),  1 - sqrt(.Machine$double.eps), length = 1001)
+            expect_equivalent(logit(x), qlogis(x))
+          }
+
+)
+
