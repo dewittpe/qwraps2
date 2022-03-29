7ccd51e2a8bd6c4429d2f0f8955d219a6ecd727c
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-03-29 10:36:42 -0600

    extend testing for n_perc

--- R/n_perc.R
+++ R/n_perc.R
@@ -13,7 +13,6 @@
 #' The function n_perc0 uses a set of default arguments which may be
 #' advantageous for use in building tables.
 #'
-#'
 #' @param x a 0:1 or boolean vector
 #' @param digits digits to the right of the decimal point to return in the
 #' percentage estimate.
@@ -21,6 +20,7 @@
 #' @param show_denom defaults to "ifNA".  Other options are "always" or "never".
 #' @param show_symbol if TRUE (default) the percent symbol is shown, else it is suppressed.
 #' @param markup latex or markdown
+#' @param ... pass through
 #'
 #' @return a character vector of the formatted values
 #'
@@ -48,7 +48,22 @@ n_perc <- function(x,
                    na_rm = FALSE,
                    show_denom = "ifNA",
                    show_symbol = TRUE,
-                   markup = getOption("qwraps2_markup", "latex")) {
+                   markup = getOption("qwraps2_markup", "latex"),
+                   ...
+                   ) {
+
+  cl <- as.list(match.call())
+  if (!("na_rm" %in% names(cl)) & ("na.rm" %in% names(cl))) {
+    na_rm <- cl$na.rm
+    warning(paste(cl[[1]], "uses the argument `na_rm`, not `na.rm`."))
+  }
+
+  stopifnot(inherits(na_rm, "logical"))
+  stopifnot(inherits(show_symbol, "logical"))
+  stopifnot(markup %in% c("latex", "markdown"))
+  stopifnot(show_denom %in% c("always", "never", "ifNA"))
+  stopifnot(inherits(x, "logical") | all(x %in% c(0, 1)))
+
   d <- sum(!is.na(x))
   n <- sum(x, na.rm = na_rm)
   p <- frmt(100 * n/d, digits)
@@ -67,7 +82,6 @@ n_perc <- function(x,
     rtn <- gsub("%", "", rtn)
   }
 
-
   if (markup == "latex") {
     rtn <- gsub("%", "\\\\%", rtn)
   }
@@ -81,7 +95,22 @@ perc_n <- function(x,
                    digits = getOption("qwraps2_frmt_digits", 2),
                    na_rm = FALSE,
                    show_denom = "ifNA",
-                   markup = getOption("qwraps2_markup", "latex")) {
+                   show_symbol = FALSE,
+                   markup = getOption("qwraps2_markup", "latex"),
+                   ...) {
+
+  cl <- as.list(match.call())
+  if (!("na_rm" %in% names(cl)) & ("na.rm" %in% names(cl))) {
+    na_rm <- cl$na.rm
+    warning(paste(cl[[1]], "uses the argument `na_rm`, not `na.rm`."))
+  }
+
+  stopifnot(inherits(na_rm, "logical"))
+  stopifnot(inherits(show_symbol, "logical"))
+  stopifnot(markup %in% c("latex", "markdown"))
+  stopifnot(show_denom %in% c("always", "never", "ifNA"))
+  stopifnot(inherits(x, "logical") | all(x %in% c(0, 1)))
+
   d <- sum(!is.na(x))
   n <- sum(x, na.rm = na_rm)
   p <- frmt(100 * n/d, digits)
@@ -90,7 +119,7 @@ perc_n <- function(x,
     rtn <- paste0(p, "% (n = ", frmt(as.integer(d)), ")")
   } else {
     if (show_denom =="always" | any(is.na(x))) {
-      rtn <- paste0(p, "% (n = ", frmt(as.integer(d)), "non-missing)")
+      rtn <- paste0(p, "% (n = ", frmt(as.integer(d)), " non-missing)")
     } else {
       rtn <- paste0(p, "% (n = ", frmt(as.integer(d)), ")")
     }
@@ -110,7 +139,21 @@ n_perc0 <- function(x,
                    na_rm = FALSE,
                    show_denom = "never",
                    show_symbol = FALSE,
-                   markup = getOption("qwraps2_markup", "latex")) {
+                   markup = getOption("qwraps2_markup", "latex"),
+                   ...) {
+
+  cl <- as.list(match.call())
+  if (!("na_rm" %in% names(cl)) & ("na.rm" %in% names(cl))) {
+    na_rm <- cl$na.rm
+    warning(paste(cl[[1]], "uses the argument `na_rm`, not `na.rm`."))
+  }
+
+  stopifnot(inherits(na_rm, "logical"))
+  stopifnot(inherits(show_symbol, "logical"))
+  stopifnot(markup %in% c("latex", "markdown"))
+  stopifnot(show_denom %in% c("always", "never", "ifNA"))
+  stopifnot(inherits(x, "logical") | all(x %in% c(0, 1)))
+
   n_perc(x, digits, na_rm, show_denom, show_symbol, markup)
 }
 
--- tests/testthat/test-n_perc.R
+++ tests/testthat/test-n_perc.R
@@ -1,7 +1,59 @@
-test_that("basic formatting", 
+test_that("basic formatting",
           {
             expect_equal(n_perc(mtcars$cyl == 6),  "7 (21.88\\%)")
             expect_equal(n_perc0(mtcars$cyl == 6), "7 (22)")
             expect_equal(perc_n(mtcars$cyl == 6), paste0("21.88\\% (n = ", nrow(mtcars), ")"))
           })
 
+test_that("errors with bad arguments",
+          {
+            expect_error(n_perc(mtcars$cyl))
+            expect_error(n_perc(mtcars$cyl == 6, na_rm = "please"))
+            expect_error(n_perc(mtcars$cyl == 6, show_denom = "please"))
+            expect_error(n_perc(mtcars$cyl == 6, show_symbol = "please"))
+            expect_error(n_perc(mtcars$cyl == 6, markup = "please"))
+
+            expect_error(n_perc0(mtcars$cyl))
+            expect_error(n_perc0(mtcars$cyl == 6, na_rm = "please"))
+            expect_error(n_perc0(mtcars$cyl == 6, show_denom = "please"))
+            expect_error(n_perc0(mtcars$cyl == 6, show_symbol = "please"))
+            expect_error(n_perc0(mtcars$cyl == 6, markup = "please"))
+
+            expect_error(perc_n(mtcars$cyl))
+            expect_error(perc_n(mtcars$cyl == 6, na_rm = "please"))
+            expect_error(perc_n(mtcars$cyl == 6, show_denom = "please"))
+            expect_error(perc_n(mtcars$cyl == 6, show_symbol = "please"))
+            expect_error(perc_n(mtcars$cyl == 6, markup = "please"))
+          })
+
+test_that("warning if na.rm used instead of na_rm",
+          {
+            expect_warning(n_perc(mtcars$cyl == 6, na.rm = TRUE))
+            expect_warning(perc_n(mtcars$cyl == 6, na.rm = TRUE))
+            expect_warning(n_perc0(mtcars$cyl == 6, na.rm = TRUE))
+          })
+
+test_that("with missing data",
+          {
+            x <- mtcars$cyl == 6
+            x[c(3, 13)] <- NA
+
+            expect_equal(n_perc(x), "NA/30 ( NA\\%)")
+            expect_equal(n_perc0(x), "NA (NA)")
+            expect_equal(perc_n(x), " NA\\% (n = 30 non-missing)")
+
+            expect_equal(n_perc(x, na_rm = TRUE), "7/30 (23.33\\%)")
+            expect_equal(n_perc0(x, na_rm = TRUE), "7 (23)")
+            expect_equal(perc_n(x, na_rm = TRUE), "23.33\\% (n = 30 non-missing)")
+
+          })
+
+test_that("show denom with no missing data",
+          {
+            x <- mtcars$cyl == 6
+
+            expect_equal(n_perc(mtcars$cyl == 6, show_denom = "always"),  "7/32 (21.88\\%)")
+            expect_equal(n_perc0(mtcars$cyl == 6, show_denom = "always"), "7/32 (22)")
+            expect_equal(perc_n(mtcars$cyl == 6, show_denom = "always"), paste0("21.88\\% (n = ", nrow(mtcars), " non-missing)"))
+
+          })
