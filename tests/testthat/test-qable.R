6fdc566c5c56d7a2c2237ef8e6aa192f71411b8a
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-03-29 08:50:54 -0600

    extend testing for qable

--- tests/testthat/test-qable.R
+++ tests/testthat/test-qable.R
@@ -4,12 +4,21 @@ test_that("simple print works",
             expect_output(print(qable(mtcars, markup = "markdown")), "142E\\ +\\|21\\.4")
           })
 
-test_that("markup language check", 
+test_that("markup language check",
           {
             expect_error(qable(mtcars, markup = "rts"))
           })
 
-test_that('rgroups', 
+test_that("rtitle",
+          {
+            out <- capture.output(qable(mtcars[1, ], rtitle = "user defined rtitle"))
+            expect_true(grepl("^user\\ defined\\ rtitle", out[4]))
+
+            out <- capture.output(qable(mtcars[1, ], markup = "markdown", rtitle = "user defined rtitle"))
+            expect_true(grepl("^\\|user\\ defined\\ rtitle", out[3]))
+          })
+
+test_that('rgroups',
           {
             make <- sub("^(\\w+)\\s?(.*)$", "\\1", rownames(mtcars))
             make <- c(table(make))
@@ -17,7 +26,7 @@ test_that('rgroups',
             expect_output(print(qable(mtcars[sort(rownames(mtcars)), ], rgroup = make)),
                           "\\\\bf\\{Volvo\\}\\ \\&\\ ~")
             expect_output(print(qable(mtcars[sort(rownames(mtcars)), ], rgroup = make)),
-                          "~~\\ Volvo\\ 142E\\ \\&\\ 21\\.4") 
+                          "~~\\ Volvo\\ 142E\\ \\&\\ 21\\.4")
 
             expect_output(print(qable(mtcars[sort(rownames(mtcars)), ], rgroup = make, markup = "markdown")),
                           "\\*\\*Volvo\\*\\*")
