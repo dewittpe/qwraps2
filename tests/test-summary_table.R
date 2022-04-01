2e08204d125663043f5c56f0f998c0d897f25a8f
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 13:04:03 -0600

    refactor unit test for summary_table

--- tests/test-summary_table.R
+++ tests/test-summary_table.R
@@ -1,22 +1,20 @@
-test_that("qsummary generates expected formulea",
-          {
-            qs <- qsummary(mtcars2[c("cyl", "cyl_factor", "cyl_character")])
+library(qwraps2)
+qs <- qsummary(mtcars2[c("cyl", "cyl_factor", "cyl_character")])
 
-            expect_equivalent(qs[["cyl"]],
-                              list(~ qwraps2::frmt(min(cyl)),
-                                   ~ qwraps2::median_iqr(cyl),
-                                   ~ qwraps2::mean_sd(cyl),
-                                   ~ qwraps2::frmt(max(cyl))))
+stopifnot(identical(qs[["cyl"]],
+                    list("minimum"      = ~ qwraps2::frmt(min(cyl)),
+                         "median (IQR)" = ~ qwraps2::median_iqr(cyl),
+                         "mean (sd)"    = ~ qwraps2::mean_sd(cyl),
+                         "maximum"      = ~ qwraps2::frmt(max(cyl)))))
 
-            expect_equivalent(qs[["cyl_factor"]],
-                              list(~ qwraps2::n_perc(cyl_factor == "6 cylinders", digits = 0, show_symbol = FALSE),
-                                   ~ qwraps2::n_perc(cyl_factor == "4 cylinders", digits = 0, show_symbol = FALSE),
-                                   ~ qwraps2::n_perc(cyl_factor == "8 cylinders", digits = 0, show_symbol = FALSE)))
+stopifnot(identical(qs[["cyl_factor"]],
+                  list("6 cylinders" = ~ qwraps2::n_perc(cyl_factor == "6 cylinders", digits = 0, show_symbol = FALSE),
+                       "4 cylinders" = ~ qwraps2::n_perc(cyl_factor == "4 cylinders", digits = 0, show_symbol = FALSE),
+                       "8 cylinders" = ~ qwraps2::n_perc(cyl_factor == "8 cylinders", digits = 0, show_symbol = FALSE))))
 
-            expect_equivalent(qs[["cyl_character"]],
-                              list(~ qwraps2::n_perc(cyl_character == "4 cylinders", digits = 0, show_symbol = FALSE),
-                                   ~ qwraps2::n_perc(cyl_character == "6 cylinders", digits = 0, show_symbol = FALSE),
-                                   ~ qwraps2::n_perc(cyl_character == "8 cylinders", digits = 0, show_symbol = FALSE)))
+stopifnot(identical(qs[["cyl_character"]],
+                  list("4 cylinders" = ~ qwraps2::n_perc(cyl_character == "4 cylinders", digits = 0, show_symbol = FALSE),
+                       "6 cylinders" = ~ qwraps2::n_perc(cyl_character == "6 cylinders", digits = 0, show_symbol = FALSE),
+                       "8 cylinders" = ~ qwraps2::n_perc(cyl_character == "8 cylinders", digits = 0, show_symbol = FALSE))))
 
-          })
 
