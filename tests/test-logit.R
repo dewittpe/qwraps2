2c7832da946c3469d5a083d797f113d72f502e03
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 12:55:45 -0600

    refactor unit test for logit

--- tests/test-logit.R
+++ tests/test-logit.R
@@ -1,26 +1,13 @@
-test_that("logit",
-          {
-            set.seed(42)
-            p <- runif(100)
+library(qwraps2)
 
-            expect_equivalent(logit(p), log(p/(1-p)))
-          })
+set.seed(42)
+p <- runif(100)
 
-test_that("invlogit",
-          {
-            set.seed(42)
-            p <- runif(100)
-            expect_equivalent(invlogit(log(p/(1-p))), p)
-          })
+stopifnot(all.equal(logit(p), log(p/(1-p))))
+stopifnot(all.equal(invlogit(log(p/(1-p))), p))
 
-test_that("logit and invlogit are equivalent to plogis",
-          {
-            x <- seq(-3, 3, length = 1001)
-            expect_equivalent(invlogit(x), plogis(x))
+x <- seq(-3, 3, length = 1001)
+stopifnot(all.equal(invlogit(x), plogis(x)))
 
-            x <- seq(sqrt(.Machine$double.eps),  1 - sqrt(.Machine$double.eps), length = 1001)
-            expect_equivalent(logit(x), qlogis(x))
-          }
-
-)
-
+x <- seq(sqrt(.Machine$double.eps),  1 - sqrt(.Machine$double.eps), length = 1001)
+stopifnot(all.equal(logit(x), qlogis(x)))
