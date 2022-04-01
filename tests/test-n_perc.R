efa157869088e3d1745f28720f8fb542f17e79cc
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 13:55:42 -0600

    refactor unit test for n_perc

--- tests/test-n_perc.R
+++ tests/test-n_perc.R
@@ -1,71 +1,51 @@
-test_that("basic formatting",
-          {
-            expect_equal(n_perc(mtcars$cyl == 6),  "7 (21.88\\%)")
-            expect_equal(n_perc0(mtcars$cyl == 6), "7 (22)")
-            expect_equal(perc_n(mtcars$cyl == 6), paste0("21.88\\% (n = ", nrow(mtcars), ")"))
-          })
+library(qwraps2)
 
-test_that("errors with bad arguments",
-          {
-            expect_error(n_perc(mtcars$cyl))
-            expect_error(n_perc(mtcars$cyl == 6, na_rm = "please"))
-            expect_error(n_perc(mtcars$cyl == 6, show_denom = "please"))
-            expect_error(n_perc(mtcars$cyl == 6, show_symbol = "please"))
-            expect_error(n_perc(mtcars$cyl == 6, markup = "please"))
+# basic formatting
+stopifnot(identical(n_perc(mtcars2$cyl == 6),  "7 (21.88\\%)"))
+stopifnot(identical(n_perc0(mtcars2$cyl == 6), "7 (22)"))
+stopifnot(identical(perc_n(mtcars2$cyl == 6), paste0("21.88\\% (n = ", nrow(mtcars2), ")")))
 
-            expect_error(n_perc0(mtcars$cyl))
-            expect_error(n_perc0(mtcars$cyl == 6, na_rm = "please"))
-            expect_error(n_perc0(mtcars$cyl == 6, show_denom = "please"))
-            expect_error(n_perc0(mtcars$cyl == 6, show_symbol = "please"))
-            expect_error(n_perc0(mtcars$cyl == 6, markup = "please"))
+# errors with bad arguments
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl), error = function(e) e), "error"))
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl == 6, na_rm = "please"), error = function(e) e), "error"))
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl == 6, show_denom = "please"), error = function(e) e), "error"))
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl == 6, show_symbol = "please"), error = function(e) e), "error"))
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl == 6, markup = "please"), error = function(e) e), "error"))
 
-            expect_error(perc_n(mtcars$cyl))
-            expect_error(perc_n(mtcars$cyl == 6, na_rm = "please"))
-            expect_error(perc_n(mtcars$cyl == 6, show_denom = "please"))
-            expect_error(perc_n(mtcars$cyl == 6, show_symbol = "please"))
-            expect_error(perc_n(mtcars$cyl == 6, markup = "please"))
-          })
 
-test_that("warning if na.rm used instead of na_rm",
-          {
-            expect_warning(n_perc(mtcars$cyl == 6, na.rm = TRUE))
-            expect_warning(perc_n(mtcars$cyl == 6, na.rm = TRUE))
-            expect_warning(n_perc0(mtcars$cyl == 6, na.rm = TRUE))
-          })
+# warning if na.rm used instead of na_rm
+stopifnot(inherits(tryCatch(n_perc(mtcars2$cyl == 6, na.rm = TRUE), warning = function(w) w), "warning"))
+stopifnot(inherits(tryCatch(perc_n(mtcars2$cyl == 6, na.rm = TRUE), warning = function(w) w), "warning"))
+stopifnot(inherits(tryCatch(n_perc0(mtcars2$cyl == 6, na.rm = TRUE), warning = function(w) w), "warning"))
 
-test_that("with missing data",
-          {
-            x <- mtcars$cyl == 6
-            x[c(3, 13)] <- NA
+# with missing data
+x <- mtcars2$cyl == 6
+x[c(3, 13)] <- NA
 
-            expect_equal(n_perc(x), "NA/30 ( NA\\%)")
-            expect_equal(n_perc0(x), "NA (NA)")
-            expect_equal(perc_n(x), " NA\\% (n = 30 non-missing)")
+stopifnot(identical(n_perc(x), "NA/30 ( NA\\%)"))
+stopifnot(identical(n_perc0(x), "NA (NA)"))
+stopifnot(identical(perc_n(x), " NA\\% (n = 30 non-missing)"))
 
-            expect_equal(n_perc(x, na_rm = TRUE), "7/30 (23.33\\%)")
-            expect_equal(n_perc0(x, na_rm = TRUE), "7 (23)")
-            expect_equal(perc_n(x, na_rm = TRUE), "23.33\\% (n = 30 non-missing)")
+stopifnot(identical(n_perc(x, na_rm = TRUE), "7/30 (23.33\\%)"))
+stopifnot(identical(n_perc0(x, na_rm = TRUE), "7 (23)"))
+stopifnot(identical(perc_n(x, na_rm = TRUE), "23.33\\% (n = 30 non-missing)"))
 
-            expect_equal(n_perc(x, na_rm = TRUE, show_denom = "never"), "7 (23.33\\%)")
-            expect_equal(n_perc0(x, na_rm = TRUE, show_denom = "never"), "7 (23)")
-            expect_equal(perc_n(x, na_rm = TRUE, show_denom = "never"), "23.33\\% (n = 30 non-missing)")
+stopifnot(identical(n_perc(x, na_rm = TRUE, show_denom = "never"), "7 (23.33\\%)"))
+stopifnot(identical(n_perc0(x, na_rm = TRUE, show_denom = "never"), "7 (23)"))
+stopifnot(identical(perc_n(x, na_rm = TRUE, show_denom = "never"), "23.33\\% (n = 30 non-missing)"))
 
-            expect_equal(n_perc(x, na_rm = TRUE, show_denom = "always"), "7/30 (23.33\\%)")
-            expect_equal(n_perc0(x, na_rm = TRUE, show_denom = "always"), "7/30 (23)")
-            expect_equal(perc_n(x, na_rm = TRUE, show_denom = "always"), "23.33\\% (n = 30 non-missing)")
+stopifnot(identical(n_perc(x, na_rm = TRUE, show_denom = "always"), "7/30 (23.33\\%)"))
+stopifnot(identical(n_perc0(x, na_rm = TRUE, show_denom = "always"), "7/30 (23)"))
+stopifnot(identical(perc_n(x, na_rm = TRUE, show_denom = "always"), "23.33\\% (n = 30 non-missing)"))
 
-            expect_equal(n_perc(x, na_rm = TRUE, show_denom = "ifNA"), "7/30 (23.33\\%)")
-            expect_equal(n_perc0(x, na_rm = TRUE, show_denom = "ifNA"), "7/30 (23)")
-            expect_equal(perc_n(x, na_rm = TRUE, show_denom = "ifNA"), "23.33\\% (n = 30 non-missing)")
+stopifnot(identical(n_perc(x, na_rm = TRUE, show_denom = "ifNA"), "7/30 (23.33\\%)"))
+stopifnot(identical(n_perc0(x, na_rm = TRUE, show_denom = "ifNA"), "7/30 (23)"))
+stopifnot(identical(perc_n(x, na_rm = TRUE, show_denom = "ifNA"), "23.33\\% (n = 30 non-missing)"))
 
-          })
+# show denom with no missing data
+x <- mtcars2$cyl == 6
 
-test_that("show denom with no missing data",
-          {
-            x <- mtcars$cyl == 6
+stopifnot(identical(n_perc(mtcars2$cyl == 6, show_denom = "always"),  "7/32 (21.88\\%)"))
+stopifnot(identical(n_perc0(mtcars2$cyl == 6, show_denom = "always"), "7/32 (22)"))
+stopifnot(identical(perc_n(mtcars2$cyl == 6, show_denom = "always"), paste0("21.88\\% (n = ", nrow(mtcars2), " non-missing)")))
 
-            expect_equal(n_perc(mtcars$cyl == 6, show_denom = "always"),  "7/32 (21.88\\%)")
-            expect_equal(n_perc0(mtcars$cyl == 6, show_denom = "always"), "7/32 (22)")
-            expect_equal(perc_n(mtcars$cyl == 6, show_denom = "always"), paste0("21.88\\% (n = ", nrow(mtcars), " non-missing)"))
-
-          })
