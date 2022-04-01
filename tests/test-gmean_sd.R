0a9752da263255f1130f5b29544e2a56c14bd3a4
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 14:17:30 -0600

    refactor testing for gmean

--- tests/test-gmean_sd.R
+++ tests/test-gmean_sd.R
@@ -1,19 +1,18 @@
-test_that("basic formatting",
-          {
-            g_mean <- exp(mean(log(mtcars$mpg)))
-            g_var  <- exp( var(log(mtcars$mpg)) * (nrow(mtcars) - 1) / nrow(mtcars))
-            g_sd   <- exp(sqrt( var(log(mtcars$mpg)) * (nrow(mtcars) - 1) / nrow(mtcars)))
-            expect_equal(gmean(mtcars$mpg), g_mean)
-            expect_equal(gvar(mtcars$mpg), g_var)
-            expect_equal(gsd(mtcars$mpg), g_sd)
+library(qwraps2)
 
-            g_mean <- frmt(g_mean)
-            g_sd   <- frmt(g_sd)
-            expect_equal(gmean_sd(mtcars$mpg), paste(g_mean, "$\\pm$", g_sd))
-          })
+# basic formatting
+g_mean <- exp(mean(log(mtcars$mpg)))
+g_var  <- exp( var(log(mtcars$mpg)) * (nrow(mtcars) - 1) / nrow(mtcars))
+g_sd   <- exp(sqrt( var(log(mtcars$mpg)) * (nrow(mtcars) - 1) / nrow(mtcars)))
 
-test_that("warning given if show_n is not as expected",
-          {
-            expect_warning(gmean_sd(mtcars$mpg, show_n = TRUE))
-          })
+stopifnot(identical(gmean(mtcars$mpg), g_mean))
+stopifnot(identical(gvar(mtcars$mpg), g_var))
+stopifnot(identical(gsd(mtcars$mpg), g_sd))
+
+g_mean <- frmt(g_mean)
+g_sd   <- frmt(g_sd)
+stopifnot(identical(gmean_sd(mtcars$mpg), paste(g_mean, "$\\pm$", g_sd)))
+
+# warning given if show_n is not as expected
+stopifnot(inherits(tryCatch(gmean_sd(mtcars$mpg, show_n = TRUE), warning = function(w) w), "warning"))
 
