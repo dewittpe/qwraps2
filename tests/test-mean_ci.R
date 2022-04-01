e1d790752e3b937cb69df0d0aca4bc35ad4ffdfe
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 17:00:09 -0600

    refactor unit tests for mean_ci

--- tests/test-mean_ci.R
+++ tests/test-mean_ci.R
@@ -1,43 +1,49 @@
-test_that("simple ci",
-          {
-            expect_equivalent(unclass(mean_ci(mtcars$mpg)),
-                              c(mean(mtcars$mpg),
-                                mean(mtcars$mpg) + qnorm(0.025)*sd(mtcars$mpg)/sqrt(nrow(mtcars)),
-                                mean(mtcars$mpg) + qnorm(0.975)*sd(mtcars$mpg)/sqrt(nrow(mtcars)))
-                              )
-          })
+library(qwraps2)
 
-test_that("Printing nicely",
-          {
-            expect_output(print(mean_ci(mtcars$mpg)), "20\\.09 \\(18\\.00, 22\\.18\\)")
-            expect_output(print(mean_ci(mtcars$mpg), show_level = TRUE), "20.09 \\(95% CI: 18\\.00, 22.18\\)")
-            expect_output(print(mean_ci(mtcars$mpg, alpha = 0.01), show_level = TRUE), "20\\.09 \\(99% CI: 17\\.35, 22\\.83\\)")
-          })
+# simple ci
+stopifnot(
+  all.equal(
+              unclass(mean_ci(mtcars$mpg))
+            ,
+              c(mean(mtcars$mpg),
+                mean(mtcars$mpg) + qnorm(0.025)*sd(mtcars$mpg)/sqrt(nrow(mtcars)),
+                mean(mtcars$mpg) + qnorm(0.975)*sd(mtcars$mpg)/sqrt(nrow(mtcars)))
+            , check.attributes = FALSE
+  )
+)
 
-test_that("Compare to the ci that comes form t.test",
-          {
-            expect_equivalent(t.test(mtcars$mpg)$conf.int,
-                              mean_ci(mtcars$mpg, qdist = stats::qt, qdist.args = list(df = 31))[2:3])
-          })
+# Printing nicely
+out <- capture.output(print(mean_ci(mtcars$mpg)))
+stopifnot(grepl("20\\.09 \\(18\\.00, 22\\.18\\)", out))
 
-test_that("simple ci with missing data",
-          {
-            x <- mtcars$mpg
-            x[c(12, 14)] <- NA
+out <- capture.output(print(mean_ci(mtcars$mpg), show_level = TRUE))
+stopifnot(grepl("20.09 \\(95% CI: 18\\.00, 22.18\\)", out))
 
-            expect_equivalent(unclass(mean_ci(x, na_rm = TRUE)),
-                              c(mean(x, na.rm = TRUE),
-                                mean(x, na.rm = TRUE) + qnorm(0.025)*sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x))),
-                                mean(x, na.rm = TRUE) + qnorm(0.975)*sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x))))
-                              )
-          })
+out <- capture.output(print(mean_ci(mtcars$mpg, alpha = 0.01), show_level = TRUE))
+stopifnot(grepl("20.09 \\(99% CI: 17\\.35, 22\\.83\\)", out))
 
-test_that("warning is thrown",
-          {
-            expect_warning(mean_ci(mtcars$mpg, na.rm = TRUE))
-          })
+# Compare to the ci that comes form t.test
+stopifnot(
+          all.equal(
+            t.test(mtcars$mpg)$conf.int,
+            mean_ci(mtcars$mpg, qdist = stats::qt, qdist.args = list(df = 31))[2:3],
+            check.attributes = FALSE
+          ))
 
-test_that("error is thrown",
-          {
-            expect_error(mean_ci(mtcars$mpg, transform = "exp"))
-          })
+# simple ci with missing data
+x <- mtcars$mpg
+x[c(12, 14)] <- NA
+
+stopifnot(
+          all.equal(unclass(mean_ci(x, na_rm = TRUE)),
+                    c(mean(x, na.rm = TRUE),
+                      mean(x, na.rm = TRUE) + qnorm(0.025)*sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x))),
+                      mean(x, na.rm = TRUE) + qnorm(0.975)*sd(x, na.rm = TRUE)/sqrt(sum(!is.na(x))))
+                    , check.attributes = FALSE)
+)
+
+# warnings
+stopifnot(inherits(tryCatch(mean_ci(mtcars$mpg, na.rm = TRUE), warning = function(w) w), "warning"))
+
+# errors
+stopifnot(inherits(tryCatch(mean_ci(mtcars$mpg, transform = "exp"), error = function(e) e), "error"))
