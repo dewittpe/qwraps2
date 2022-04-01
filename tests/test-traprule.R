14ef0cbe73753463010ee0cd8f2712e8bf76591e
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-04-01 12:50:53 -0600

    refactor unit test for traprule

--- tests/test-traprule.R
+++ tests/test-traprule.R
@@ -1,17 +1,14 @@
-test_that("traprule",
-          {
-            expect_equal(traprule(x = c(0, 0, 1, 1), y = c(0, 1, 1, 0)), 1.0)
-            expect_equal(traprule(x = c(0, 0, 1, 1), y = c(0, 2, 1, 0)), 1.5)
+library(qwraps2)
 
-            two_to_the_x <- function(x) {
-              2 ** x
-            }
+stopifnot(identical(traprule(x = c(0, 0, 1, 1), y = c(0, 1, 1, 0)), 1.0))
+stopifnot(identical(traprule(x = c(0, 0, 1, 1), y = c(0, 2, 1, 0)), 1.5))
 
-            x <- seq(-1, 3, by = 1)
-            expect_equal(traprule(x, two_to_the_x(x)), 11.25)
+two_to_the_x <- function(x) {
+  2 ** x
+}
 
-            x <- seq(-1, 3, length = 100)
-            expect_equal(sprintf("%.16f", traprule(x, two_to_the_x(x))), "10.8209200182984446")
+x <- seq(-1, 3, by = 1)
+stopifnot(identical(traprule(x, two_to_the_x(x)), 11.25))
 
-          }
-)
+x <- seq(-1, 3, length = 100)
+stopifnot(identical(sprintf("%.16f", traprule(x, two_to_the_x(x))), "10.8209200182984446"))
