ff26dcc2092cea71a51b648d3c03fa93cbd2875d
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2023-11-03 17:21:01 -0600

    wip - file-check.R extention; testing fails on this commit

--- R/file-check.R
+++ R/file-check.R
@@ -8,68 +8,98 @@
 #' current user.
 #'
 #' The return of the function is \code{TRUE} if all the files in \code{paths}
-#' are accessible and all of requested md5sum checks pass.  \code{FALSE} is any
-#' file is not accessible or any md5sum check fails.  By default, if the return
-#' is \code{TRUE} then only \code{TRUE} will be printed to the console.  If the
-#' return is \code{FALSE} then the \code{attr(, "checks")} is printed by default
-#' as well.
+#' are accessible, are case matched (optional), and all of requested md5sum
+#' checks pass.  Windows and macOS are generally case-insensitive systems, but
+#' many Linux systems are case-sensitive.  As such
+#' \code{\link[base]{file.exists}} and \code{\link[base]{file.access}} may
+#' return different values depending the OS that is active.  \code{file_check}
+#' looks for a case match as part of its checks to hopefully prevent issues
+#' accross operating systems.
+#'
+#' By default, if the return is \code{TRUE} then only \code{TRUE} will
+#' be printed to the console.  If the return is \code{FALSE} then the
+#' \code{attr(, "checks")} is printed by default as well.
 #'
 #' Good practice would be to use relative paths, a warning will be given if any
-#' of the \code{paths} are determined to be absolute paths.
+#' of the \code{paths} are determined to be absolute paths.  That said, there
+#' are cases when an absolute path is needed, e.g., a common data file on a
+#' server with multiple users accessing the file(s).  Set \code{absolute_paths =
+#' c("silent")} to silence the warnings.
 #'
 #' @param paths a character path to the target file
 #' @param md5sums a character string for the expected md5sum of the target file.
 #' If \code{NULL} then only a \code{file.exists} check will be done.
-#' @param stop if \code{TRUE} then an error is thrown if any of the checks fail.
-#' If \code{FALSE} (default) a logical is returned.
+#' @param absolute_paths a character string to set the behavior of warning
+#' (default), stopping, or silent if/when absolute file paths are used.
+#' @param stop if \code{TRUE} then an error is thrown if any of the checks fail,
+#' else returns a logical. If \code{FALSE} (default) a logical is returned.
 #'
 #' @return The function will return a single TRUE/FALSE value with attributes
 #' \code{attr(, "checks")}.
 #'
 #'
 #' @examples
-#' # create two example files in the working directory:
-#' cat("example file.", file = "QWRAPS2_EXAMPLE_1.txt")
-#' cat("Another example file.", file = "QWRAPS2_EXAMPLE_2.txt")
+#' # create example files
 #'
-#' # Check that you have access to these files:  (Should return TRUE)
-#' test1 <- file_check(c("QWRAPS2_EXAMPLE_1.txt", "QWRAPS2_EXAMPLE_2.txt"))
+#' relative_example_file1 <- basename(tempfile(pattern = "QWRAPS2_EXAMPLE_1", fileext = ".txt", tmpdir = getwd()))
+#' relative_example_file2 <- basename(tempfile(pattern = "QWRAPS2_EXAMPLE_2", fileext = ".txt", tmpdir = getwd()))
+#' absolute_example_file  <- tempfile()
+#' 
+#' cat("example file.", file = relative_example_file1)
+#' cat("Another example file.", file = relative_example_file2)
+#' cat("Another example file.", file = absolute_example_file)
+#'
+#' # Check that you have access to the files in the working directory.
+#' test1 <- file_check(c(relative_example_file1, relative_example_file2))
 #' test1
 #'
 #' # By default, when the checks return TRUE the details of the checks are not
 #' # printed.  You can view the details of the checks as follows:
 #' attr(test1, "checks")
 #'
+#' # access to absolute_example_file will generate a warning about
+#' # absolute_paths by default
+#' file_check(absolute_example_file)
+#' file_check(absolute_example_file, absolute_paths = "silent")
+#'
+#' # Case Match
+#' test_case_match <- file_check(c(relative_example_file1, tolower(relative_example_file1)))
+#' test_case_match
+#'
 #' # If one or more files is not accessable then return is FALSE and the meta data
 #' # is printed by default.
-#' test2 <- file_check(c("UNLIKELYFILENAME", "QWRAPS2_EXAMPLE_1.txt", "QWRAPS2_EXAMPLE_2.txt"))
-#' test2
+#' test_non_existent_file <- file_check(c("UNLIKELYFILENAME", relative_example_file1, relative_example_file2))
+#' test_non_existent_file
 #'
 #' # Or have an error thrown:
 #' \dontrun{
-#' file_check(c("UNLIKELYFILENAME", "QWRAPS2_EXAMPLE_1.txt", "QWRAPS2_EXAMPLE_2.txt"),
-#'            stop = TRUE)
+#' file_check(c("UNLIKELYFILENAME", relative_example_file1, relative_example_file2), stop = TRUE)
 #' }
 #'
 #' # Verify the md5sums as well as file access:
-#' file_check("QWRAPS2_EXAMPLE_1.txt", "7a3409e17f9de067740e64448a86e708")
+#' md5_check1 <- file_check(relative_example_file1, "7a3409e17f9de067740e64448a86e708")
+#' md5_check1
 #'
 #' # If you only need to verify a subset of md5sums then use an NA in the md5sums
 #' # argument:
-#' file_check(c("QWRAPS2_EXAMPLE_1.txt", "QWRAPS2_EXAMPLE_2.txt"),
-#'            c("7a3409e17f9de067740e64448a86e708", NA))
+#' md5_check2 <-
+#'   file_check(c(relative_example_file1, relative_example_file2),
+#'              c("7a3409e17f9de067740e64448a86e708", NA))
+#' md5_check2
 #'
 #' # Verify all the md5sums
-#' file_check(c("QWRAPS2_EXAMPLE_1.txt", "QWRAPS2_EXAMPLE_2.txt"),
-#'            c("7a3409e17f9de067740e64448a86e708", "798e52b92e0ae0e60f3f3db1273235d0"))
-#'
+#' md5_check3 <-
+#'   file_check(c(relative_example_file1, relative_example_file2),
+#'              c("7a3409e17f9de067740e64448a86e708", "798e52b92e0ae0e60f3f3db1273235d0"))
+#' md5_check3
 #'
 #' # clean up working directory
-#' unlink("QWRAPS2_EXAMPLE_1.txt")
-#' unlink("QWRAPS2_EXAMPLE_2.txt")
+#' unlink(relative_example_file1)
+#' unlink(relative_example_file2)
+#' unlink(absolute_example_file)
 #'
 #' @export
-file_check <- function(paths, md5sums = NULL, stop = FALSE) {
+file_check <- function(paths, md5sums = NULL, absolute_paths = c("warn", "stop", "silent"), stop = FALSE) {
   paths <- as.character(paths)
   md5sums <- as.character(md5sums)
   current_md5sums <- tools::md5sum(path.expand(paths))
@@ -83,35 +113,46 @@ file_check <- function(paths, md5sums = NULL, stop = FALSE) {
     md5check[is.na(md5sums)] <- NA
   }
 
-  # Where the files paths absolute or relative?
-  absolute_paths <- regexpr("^((~|/)|.:(/|\\\\))", paths) != -1L
-
-  if (any(absolute_paths)) {
-    warning("Absolute path used.  It would be preferable if you used a relative path.")
+  # are files paths absolute or relative?
+  action <- match.arg(absolute_paths)
+  abspths <- NA
+  if (action %in% c("warn", "stop")) {
+    abspths <- regexpr("^((~|/)|.:(/|\\\\))", paths) != -1L
+    if (any(abspths)) {
+      if (action == "warn") {
+        warning("Absolute path used. It would be preferable if you used a relative path.")
+      } else {
+        stop("Absolute path used. It would be preferable if you used a relative path.")
+      }
+    }
   }
 
+  case_match <-
+    sapply(paths, function(x) {length(list.files(dirname(x), pattern = basename(x), ignore.case = FALSE)) == 1L})
+
   checks <-
-    data.frame(path          = paths,
-               absolute_path = absolute_paths,
-               accessible    = file.access(paths, mode = 4) != -1L,
-               current_md5sum = current_md5sums,
-               expected_md5sum = NA_character_,
-               md5check = md5check,
-               stringsAsFactors = FALSE)
+    data.frame(
+               path          = paths
+               , absolute_path = abspths
+               , accessible    = file.access(paths, mode = 4) != -1L
+               , case_match    = case_match
+               , current_md5sum = current_md5sums
+               , expected_md5sum = NA_character_
+               , md5check = md5check
+               , stringsAsFactors = FALSE
+    )
 
   if (length(md5sums) > 0L) {
     checks$expected_md5sum <- md5sums
   }
 
-  checks
-
-  status <- all(c(checks$accessible, stats::na.omit(checks$md5check)))
+  status <- all(c(checks$accessible, checks$case_match, stats::na.omit(checks$md5check)))
   attr(status, "checks") <- checks
   attr(status, "class") <- "qwraps2_file_check"
 
   if (!status && stop) {
     print(status)
-    stop("At least one path or md5sum check failed.", call. = FALSE)
+    stop("At least one path, case match, or md5sum check failed.", call. = FALSE)
   }
 
   status
